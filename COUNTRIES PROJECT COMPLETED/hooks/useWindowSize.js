e37fd5668df996302a4e31ae43344e56dd4e6b0b
pranitcivil <pranitshinde68@gmail.com>
2024-09-10 19:39:43 +0530

    Add files via upload

---
+++ COUNTRIES PROJECT COMPLETED/hooks/useWindowSize.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export function useWindowSize() {
+    const [windowsize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        });
+    }, []);
+
+    return windowsize
+
+}
